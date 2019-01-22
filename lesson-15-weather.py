4fb3704e95b8000147d1f67dcbc837e72f41c911
Rafael Leal <rafaellx10@gmail.com>
2019-01-21 23:11:32 -0200

    First commit

---
+++ lesson-15-weather.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+#cidade = input('What is your city: ')
+cidade = input('Qual a sua cidade: ')
+
+req = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+ cidade + '&appid=8ba5aae0a1dd25f3de5d91a05013388c')
+tempo = json.loads(req.text)
+
+
+#print(tempo)
+
+
+print('Condição do tempo:',tempo["weather"][0]["main"])
+print('temperatura:{:.2f}'.format(float(tempo["main"]["temp"]) - 273.15))
